ec67fc6741069263978ac2651fbbb6be64657fbe
mugg84 <56695670+mugg84@users.noreply.github.com>
2020-05-06 16:41:04 +0100

    add axios instead of command fetch, API request for recipe

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,29 @@
-import string from './modules/Search';
+/*
+const res = await axios(`${PROXY}http://food2fork.com/api/search?key=${KEY}&q=${this.query}`);
+replace top with 
+const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
 
-import {
-        add as a,
-        multiply,
-        ID
-} from './view/searchView'
+in recipe.js
 
-console.log(`Using imported functions ${a(ID, 2)}, plus ${string}`);
+const res = await axios(`${PROXY}http://food2fork.com/api/get?key=${KEY}&rId=${this.id}`);
+
+replaced with
+const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+*/
+
+//https://forkify-api.herokuapp.com/api/search
+
+import axios from 'axios';
+
+async function getResults(query) {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+
+        try {
+                const recipes = res.data.recipe;
+                console.log(res);
+        } catch (error) {
+                console.log(error);
+        }
+}
+
+getResults('pasta');
