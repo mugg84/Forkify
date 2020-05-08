57898f59841d59ce5c9bd3ad150b91ed641306fe
mugg84 <56695670+mugg84@users.noreply.github.com>
2020-05-08 14:51:57 +0100

    restart

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,1 @@
-import string from './modules/Search';
-
-import {
-        add as a,
-        multiply,
-        ID
-} from './view/searchView'
-
-console.log(`Using imported functions ${a(ID, 2)}, plus ${string}`);
+// Global app controller
