fa41c90c8714a296c0c460125d3fbe4c4190eba0
mugg84 <56695670+mugg84@users.noreply.github.com>
2020-05-08 16:25:30 +0100

    install webpack and test

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
 // Global app controller
+// entry: ['./src/js/index.js'], webpack config
+
+/*
+{
+        "presets": [
+                ["@babel/env", {
+                        "useBuiltIns": "usage",
+                        "corejs": "3",
+                        "targets": {
+                                "browsers": [
+                                        "last 5 versions",
+                                        "ie >= 8"
+                                ]
+                        }
+                }]
+        ]
+}
+
+*/
+
+
+import num from './test';
+
+console.log(num)
