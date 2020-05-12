4f7bde3f6c62cdafa91bb89a551789aace4eab89
mugg84 <56695670+mugg84@users.noreply.github.com>
2020-05-12 14:43:59 +0100

    babel install and setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,3 @@
-// Global app controller
-// entry: ['./src/js/index.js'], webpack config
-
-/*
-{
-        "presets": [
-                ["@babel/env", {
-                        "useBuiltIns": "usage",
-                        "corejs": "3",
-                        "targets": {
-                                "browsers": [
-                                        "last 5 versions",
-                                        "ie >= 8"
-                                ]
-                        }
-                }]
-        ]
-}
-
-*/
-
 import num from "./test";
-
-console.log(num);
+const x = 23;
+console.log(`variable x is ${x}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ["./src/js/index.js"],
+  entry: ["babel-polyfill", "./src/js/index.js"],
   output: {
     path: path.resolve(__dirname, "dist/js"),
     filename: "bundle.js",
@@ -11,9 +11,20 @@ module.exports = {
     contentBase: "./dist",
   },
   plugins: [
-    new HTMLWebpackPlugin({
+    new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./src/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
 };
