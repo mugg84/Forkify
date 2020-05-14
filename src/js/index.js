8525eb0041da809d2239070244de8ae3914fd1a5
mugg84 <56695670+mugg84@users.noreply.github.com>
2020-05-14 17:24:19 +0100

    impemented Seach component, started State object in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,31 @@
 import Search from "./modules/Search";
 
-const search = new Search("pizza");
-console.log(search);
+/* Global state of the app
+-Seach obj
+-Current recipe obj
+-liked recipes
+*/
 
-search.getResults();
+const state = {};
+
+const controlSearch = async () => {
+  // 1) get query from view
+  const query = "pizza"; //later
+
+  if (query) {
+    //2) new search obj and add to state
+    state.search = new Search(query);
+    //3) prepare ui for results
+
+    //4) search for recipes
+    await state.search.getResults();
+
+    //5) render results on ui
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
